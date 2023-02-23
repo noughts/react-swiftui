07dcd9ffb33977382c8cc1bd14f1fa0039e29316
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2023-02-23 11:23:00 +0900

    a

---
+++ dist/components/HStack.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+export default class HStack extends React.Component {
+    render(): React.ReactNode;
+}
---
+++ src/components/HStack.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default class HStack extends React.Component{
+	render(): React.ReactNode {
+		return <div>this is HStack</div>
+	}
+}
