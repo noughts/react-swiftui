d25041d7a09542c8a528960aaf0224f2a3672e17
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-05-08 18:18:13 +0900

    a

--- src/demo/NavigationViewDemo.tsx
+++ src/demo/NavigationViewDemo.tsx
@@ -10,17 +10,35 @@ export default class NavigationDemo extends React.Component {
 	}
 }
 
-class Page1 extends React.Component<ViewProps>{
+class Page1 extends React.Component<ViewProps,{
+	counter:number;
+}>{
+
 
 	static contextType = NavigationContext;
 	declare context: React.ContextType<typeof NavigationContext>;
 
+	constructor(p){
+		super(p);
+		console.log("Page1 constructor")
+		this.state = {
+			counter: 0,
+		}
+	}
+
 	render(): React.ReactNode {
 		return <div>
 			<div>Page1</div>
+			<div>{this.state.counter}</div>
 			<Button label="push" action={e => {
 				this.context.push(<Page2 />);
 			}} />
+
+			<Button label="plus" action={e=>{
+				const newCount = this.state.counter + 1;
+				this.setState({counter:newCount});
+			}}/>
+
 		</div>
 	}
 }
